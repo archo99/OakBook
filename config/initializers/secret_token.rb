2d248085341491749eed9034b339ff6d9717f43d
Archie Wheeler <mrarchiewheeler@gmail.com>
2013-04-10 15:20:56 +0100

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OakBook::Application.config.secret_token = 'a429140296fd75a6d68fed28171f4e1fddeb8854cd38d8c33cbeaa629f53324a1d21756ac9424fa947682ba4e5dd9cdaf85b9941b2d252100d6a474c1f96eef9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OakBook::Application.config.session_store :cookie_store, key: '_OakBook_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OakBook::Application.config.session_store :active_record_store
